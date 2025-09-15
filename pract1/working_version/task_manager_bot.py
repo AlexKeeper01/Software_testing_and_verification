da930eea4e73ded5d49a000dcae5e6bde6668f9e
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2025-09-15 19:44:11 +0300

    Update task_manager_bot.py

--- pract1/working_version/task_manager_bot.py
+++ pract1/working_version/task_manager_bot.py
@@ -37,7 +37,6 @@ def save_data(data: Dict[str, List[dict]]):
     with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-
 class AddState(StatesGroup):
     waiting_for_text = State()
 
